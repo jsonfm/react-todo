d998ca3b67672fcdde7a9609030b1c4b565b45fc
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-21 14:05:14 -0500

    🎨 [UPDATE] Task Item Styles

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,11 +1,19 @@
 import "./styles.css";
-
+import { UilCheckSquare } from '@iconscout/react-unicons';
+import { UilSquareFull } from '@iconscout/react-unicons';
+import { UilTrash } from '@iconscout/react-unicons';
 
 export const TodoItem = ({ text, completed }) => {
+
     return(
         <div className="task">
-            {completed}
+            <div className="check">
+                {completed ? <UilCheckSquare/>: <UilSquareFull/>}
+            </div>
             <p>{text}</p>
+            <div className="trash">
+                <UilTrash/>
+            </div>
         </div>
     )
 }
