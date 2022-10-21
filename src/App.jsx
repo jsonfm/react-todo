a8acb5f2ed5a6d3592c108dbd390e2dcc464ef2f
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-10-21 12:10:58 -0500

    ⚡ [ADD] Todos Service

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
+
+
+
+// Components
 import { TodoCounter } from "./components/TodoCounter";
 import { TodoSearch } from "./components/TodoSearch";
 import { TodoList } from "./components/TodoList";
 import { TodoItem } from "./components/TodoItem";
 
-const todos = [
-  {
-    text: 'Cortar cebolla',
-    completed: true,
-  },
-  {
-    text: 'Tomar el curso de React',
-    completed: false,
-  },
-  {
-    text: 'Llorar con la llorona',
-    completed: true,
-  },
+// Services
+import { TodosService } from "./services/TodosService";
 
-]
+const todosService = new TodosService();
+const todos = todosService.getTodos();
 
 function App() {
   return (
---
+++ src/services/TodosService/index.jsx
@@ -0,0 +1,50 @@
+const todos = [
+    {
+      text: 'Cortar cebolla',
+      completed: true,
+    },
+    {
+      text: 'Tomar el curso de React',
+      completed: false,
+    },
+    {
+      text: 'Llorar con la llorona',
+      completed: true,
+    },
+]
+
+export class TodosService {
+    constructor(){
+        this.todos = todos;
+    }
+
+    async sleep(ms=1000){
+      return await new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async fetchTodos(delay=1000){
+      await sleep(delay);
+      return this.todos;
+    }
+
+    getTodos() {
+        return this.todos;
+    }
+
+    getCompletedTodos() {
+        return this.todos.filter(todo => todo.completed);
+    }
+
+    search(query) {
+        return this.todos.filter(todo => todo.toLowerCase().includes(query.toLowerCase()));
+    }
+
+    update(){
+      return;
+    }
+
+    delete(id){
+      return;
+    }
+
+}
